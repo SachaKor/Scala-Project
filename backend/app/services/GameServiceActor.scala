cd66402aa555d7a4c8dcfd10d2cfb4fbe8c8b02d
SashaKor <alexandra.korukova@gmail.com>
2019-06-09 18:48:12 +0200

    little changes

--- backend/app/services/GameServiceActor.scala
+++ backend/app/services/GameServiceActor.scala
@@ -3,17 +3,24 @@ package services
 import akka.actor.{Actor, ActorRef, PoisonPill, Props}
 import models._
 import play.api.Logger
+import play.api.libs.json.{JsNumber, JsValue, Json}
 
 object GameServiceActor {
   def props(out: ActorRef, user: User) = Props(new GameServiceActor(out, user))
 }
 
+
 class GameServiceActor(out: ActorRef, user: User) extends Actor {
+  def playerJoined(): JsValue = Json.obj(
+    "nb-players:" -> Game.players.length
+  )
+
   override def receive: Receive = {
     case msg: InEvent => {
       msg.eventType match {
         case "join" => {
           Logger.debug("JOIN EVENT")
+          Game.addPlayer(new Player(user.username, List()))
           out ! new OutEvent("joined")
         }
         case "leave" => {
