e22a4dafa14d4ef87d5d10168ef5c7a85d3b7b27
psr <pierre.samuel.rochat@gmail.com>
2019-06-10 15:01:22 +0200

    socket frontend

--- frontend/src/App/components/pages/BoardPage.js
+++ frontend/src/App/components/pages/BoardPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import SocketContext from '../../utils/SocketContext'
 import Deck from '../deck/Deck'
 import CommonDeck from '../deck/CommonDeck'
 
@@ -13,6 +13,37 @@ class BoardPage extends Component {
   constructor(props) {
     super(props)
     this.state = INITIAL_STATE
+  }
+
+  connectSocket = () => {
+    const user = JSON.parse(localStorage.getItem('user'))
+    const { setSocket } = this.props
+
+    const URL = 'ws://localhost:9000/ws?token=' + user.token
+
+    const socket = new WebSocket(URL)
+
+    this.setState({
+      socket
+    }, () => setSocket(this.state.socket))
+
+    socket.onopen = () => {
+      console.log('connected')
+    }
+
+    socket.onmessage = this.handleMessages
+
+    socket.onclose = (e) => {
+      console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+      setTimeout(() => {
+        this.connectSocket()
+      }, 1000)
+    }
+
+    socket.onerror = (err) => {
+      console.error('Socket encountered error: ', err.message, 'Closing socket');
+      socket.close()
+    }
   }
 
   render() {
@@ -37,4 +68,10 @@ class BoardPage extends Component {
   }
 }
 
-export default BoardPage
+const BoardPageWithSocket = props => (
+  <SocketContext.Consumer>
+    {socket => <BoardPage {...props} socket={socket} />}
+  </SocketContext.Consumer>
+)
+
+export default BoardPageWithSocket
